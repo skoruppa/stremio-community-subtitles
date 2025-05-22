98eaa33b8b0e5d85812ad85d51a397b6ca8a573d
skoruppa <skoruppa@gmail.com>
2025-05-23 00:28:12 +0200

    change session_protection to basic

--- app/extensions.py
+++ app/extensions.py
@@ -20,4 +20,4 @@ mail = Mail()
 login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Please log in to access this page.'
 login_manager.login_message_category = 'info'
-login_manager.session_protection = 'strong'
+login_manager.session_protection = 'basic'
